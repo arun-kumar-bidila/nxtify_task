3ea7ea631d98cf883565fed4c64361ec91f0a5d4
Arun Kumar Bidila <arunkumarbidila@gmail.com>
2025-09-11 13:05:17 +0530

    Ready for deployment stage two

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: "https://nxtify-task.vercel.app/",
+  origin: "https://nxtify-task.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"], 
 }));
 
